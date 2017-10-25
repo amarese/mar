1fe2543291d00e0a7f4524e125857f0acd960f84
Shawn Lee <amarese@naver.com>
2017-10-26 01:04:24 +0900

    avoid npe when main article image is null

--- writer/src/main/java/pe/mar/writer/news/collector/NaverNewsCollector.java
+++ writer/src/main/java/pe/mar/writer/news/collector/NaverNewsCollector.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.google.common.collect.Lists;
 
 import lombok.Getter;
+import pe.mar.common.utils.IsNotEmpty;
 
 @Service
 public class NaverNewsCollector extends NewsCollectorBase {
@@ -38,21 +39,26 @@ public class NaverNewsCollector extends NewsCollectorBase {
 	}
 
 	Article mainArticle(String cutString) {
-		Pattern pattern = Pattern.compile(
-				"<dt class=\"photo\">.+?<img src=\"(.+?)\".+?<dt>[\\s]*<a.+?href=\"(.+?)\" >[\\s]*(.+?)</a>.+?<dd.+?>(.+?)<span class=\"writing\">(.+?)</span>",
-				Pattern.DOTALL);
-		Matcher matcher = pattern.matcher(cutString);
-		if (matcher.find()) {
-			String image = matcher.group(1);
-			String link = matcher.group(2);
-			String title = matcher.group(3);
-			String content = matcher.group(4);
-			String medium = matcher.group(5);
 
-			Article mainArticle = new Article(medium, title, content, link, image);
-			return mainArticle;
+		Pattern pattern = Pattern.compile("<dt class=\"photo\">.+?<img src=\"(.+?)\".+?", Pattern.DOTALL);
+		Matcher matcher = pattern.matcher(cutString);
+		String image = "";
+		if (matcher.find()) {
+			image = matcher.group(1);
 		}
-		return null;
+		Pattern pattern2 = Pattern.compile(
+				"<dt>[\\s]*<a.+?href=\"(.+?)\" >[\\s]*(.+?)</a>.+?<dd.+?>(.+?)<span class=\"writing\">(.+?)</span>",
+				Pattern.DOTALL);
+		Matcher matcher2 = pattern2.matcher(cutString);
+		matcher2.find();
+		String link = matcher2.group(1);
+		String title = matcher2.group(2);
+		String content = matcher2.group(3);
+		String medium = matcher2.group(4);
+
+		Article mainArticle = new Article(medium, title, content, link, image);
+		return mainArticle;
+
 	}
 
 	List<Article> subArticles(String cutString) {
@@ -81,10 +87,17 @@ public class NaverNewsCollector extends NewsCollectorBase {
 		String intro = String.format("현재 국제사회에서 \"<a href=\"%s\">%s</a>\" 이슈가 화제입니다.<br>", news.getLink(),
 				news.getTitle());
 		Article mainArticle = news.getMainArticle();
+
+		String image = IsNotEmpty.string(mainArticle.getImage())
+				? String.format("<a href=\"%s\"><img src=\"%s\" align=\"left\"></a>", mainArticle.getLink(),
+						mainArticle.getImage())
+				: "";
+
 		String main = String.format(
-				"최근 %s 에서는  \"<a href=\"%s\">%s</a>\" 의 제목으로 아래와 같은 보도를 전했습니다.<div><a href=\"%s\"><img src=\"%s\" align=\"left\"></a>%s</div>",
-				mainArticle.getMedium(), mainArticle.getLink(), mainArticle.getTitle(), mainArticle.getLink(),
-				mainArticle.getImage(), mainArticle.getContent());
+				"<p>최근 %s 에서는  \"<a href=\"%s\">%s</a>\" 의 제목으로 아래와 같은 보도를 전했습니다.</p>" + "%s" + "%s",
+				mainArticle.getMedium(), mainArticle.getLink(), mainArticle.getTitle(), image,
+				mainArticle.getContent());
+
 		String sub = "<br><br>그 외에도 각종 언론사들은 다음과 같은 보도를 이어나갔습니다.<br><br>";
 		for (Article article : news.getSubArticles()) {
 			sub += String.format("<a href=\"%s\">%s</a> - %s<br>\n", article.getLink(), article.getTitle(),
